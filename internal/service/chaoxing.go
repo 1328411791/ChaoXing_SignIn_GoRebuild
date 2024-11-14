d65eb992edb2aa202e3b6fb698a528525200f440
lihanyu <1328411791@qq.com>
2024-11-15 05:34:48 +0800

    feat: 注册登录接口

--- internal/service/chaoxing.go
+++ internal/service/chaoxing.go
@@ -1,8 +1,23 @@
-// ================================================================================
-// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
-// You can delete these comments if you wish manually maintain this interface file.
-// ================================================================================
-
 package service
 
-type ()
+import (
+	"log"
+)
+
+type IChaoxing interface {
+	UserLogin(phone string, password string) (err error)
+}
+
+type Chaoxing struct {
+}
+
+func UserLogin(phone string, password string) (err error) {
+	log.Println("User Login")
+
+	// 查询用户是否存在
+	//g.DB().Model("chaoxing").Where("phone =?", phone).Scan(&chaoxing.Chaoxing{})
+
+	//login, err := chaoxing.UserLogin(phone, password)
+
+	return nil
+}
